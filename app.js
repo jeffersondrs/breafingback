705b95bb11911cd6bb9b6b1ac5c6064e11da8aa3
jeffersondrs <jeffersonduarte@id.uff.br>
2022-12-20 22:56:42 -0300

    fixing cors packcage

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
+const serverless = require("serverless-http");
 const router = require("./src/routes/breafingRouters");
 
 const app = express();
@@ -12,3 +13,4 @@ app.use(express.json());
 app.use("/api/breafing", router);
 
 module.exports = app;
+module.exports.handler = serverless(app);
